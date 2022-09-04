14e4ba450a8514cafc720e30a49ce5bb997cbb75
Alex Vencel <a.vencel@orange.fr>
2022-09-04 17:45:03 -0500

    documentation added for utilities

--- src/components/dropzone/utils.ts
+++ src/components/dropzone/utils.ts
@@ -1,3 +1,4 @@
+/** All the dropzone error codes and their default error messages */
 export const ERROR_CODES = {
   TOO_MANY_FILES: 'too-many-files',
   FILE_TOO_LARGE: 'file-too-large',
@@ -7,6 +8,14 @@ export const ERROR_CODES = {
   INVALID_FILE_TYPE_MESSAGE: () => 'Invalid file type.',
 };
 
+/** 
+ *  @function splitFileExtension
+ *  Splits the file name and extension. Truncate the file if truncation provided
+ *  @param {string} name - The file name
+ *  @param {number} [trunctation] - The number of characters 
+ *  to truncate the file name. Will add ... at the end
+ *  @return {{filename: string, fileExtension: string}} The file name and extension
+ */
 export const splitFileExtension = (name: string, truncation?: number) => {
   let [fileName, fileExtension] = name.split('.');
   fileName.length > (truncation || fileName.length + 1)
@@ -15,11 +24,27 @@ export const splitFileExtension = (name: string, truncation?: number) => {
   return { fileName, fileExtension };
 }
 
+/** 
+ * @function formatFileText
+ * Formats the file name and extension. Truncate the file if truncation provided
+ * @param {string} name - The file name
+ * @param {number} [trunctation] - The number of characters
+ * to truncate the file name. Will add ... at the end
+ * @return {string} The formatted file name
+ */
 export const formatFileText = (name: string, truncation?: number) => {
   const { fileName, fileExtension } = splitFileExtension(name, truncation);
   return `${fileName} (${fileExtension})`;
 }
 
+/**
+ * @function filesWithoutDuplicates
+ * Filters out duplicate files
+ * @param {File[]} toFilter - The files array to use as base of filtering
+ * and remove the duplicates from
+ * @param {File[]} intersection - The files array to interect with
+ * @return {File[]} The filtered files array
+ */
 export const filesWithoutDuplicates = (toFilter: File[], intersecting: File[]) => {
   return toFilter.filter((file: File) =>
     !intersecting.find((otherFile: File) =>
@@ -28,6 +53,14 @@ export const filesWithoutDuplicates = (toFilter: File[], intersecting: File[]) =
   );
 }
 
+/**
+ * @function getCodeMessage
+ * Returns the error message for the given error code
+ * @param {string} code - The error code
+ * @param {number} [maxSize] - The maximum file size in bytes
+ * @param {number} [maxFiles] - The maximum number of files
+ * @return {string} The error message
+ */
 export const getCodeMessage = (code: string, maxSize: number, maxFiles: number) => {
   switch (code) {
     case ERROR_CODES.INVALID_FILE_TYPE:
@@ -41,6 +74,16 @@ export const getCodeMessage = (code: string, maxSize: number, maxFiles: number) 
   }
 };
 
+/**
+ * @function formatBytes
+ * Formats the bytes to a human readable format
+ * @param {number} bytes - The number of bytes
+ * @param {number} [decimals] - The number of decimals to use
+ * @return {string} The formatted bytes
+ * @notice
+ * Goes from bytes to yottabytes
+ * @see https://stackoverflow.com/a/18650828/10090458
+ */
 export const formatBytes = (bytes: number, decimals = 2) => {
   if (bytes === 0) return '0 Bytes';
   const k = 1024;
@@ -50,6 +93,14 @@ export const formatBytes = (bytes: number, decimals = 2) => {
   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 }
 
+/**
+ * @function getBytes
+ * Converts the amount in the given unit to bytes
+ * @param {number} amount - The amount
+ * @param {string} unit - The unit
+ * @return {number} The amount in bytes
+ * @example getBytes(1, 'MB') returns 1048576
+ */
 export const getBytes = (amount: number, format: string) => {
   const sizes = ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
   const index = sizes.indexOf(format);
