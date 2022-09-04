7d69228a5f274746d295b6e71d13c06c025c8d03
Alex Vencel <a.vencel@orange.fr>
2022-09-04 17:24:54 -0500

    added documentation to dropzone-context

--- src/components/dropzone/dropzone-context.tsx
+++ src/components/dropzone/dropzone-context.tsx
@@ -1,11 +1,23 @@
 import { createContext } from "@utils/context/createContext";
 
+/** The various dropzone statuses
+ *  - Base: the dropzone is ready to receive files
+ *  - Accept: drop in progress, files should be accepted
+ *  - Reject: drop in progress, files should be rejected
+ */
 export interface DropzoneStatuses {
   Base: boolean;
   Accept: boolean;
   Reject: boolean;
 }
 
+/** The values stored in the dropzone context 
+ * - all the statuses (DropzoneStatuses)
+ * - Files: the files currently in the dropzone
+ * - setFiles: a function to set the files in the dropzone
+ * - Disabled: whether the dropzone is disabled
+ * - Animated: whether the dropzone is animated
+ */
 export interface DropzoneContextValue extends DropzoneStatuses {
   Files: File[];
   setFiles: (files: File[]) => void;
@@ -13,6 +25,7 @@ export interface DropzoneContextValue extends DropzoneStatuses {
   Animated: boolean;
 };
 
+/** Creates the dropzone context provider */
 export const [DropzoneProvider, useDropzoneContext] =
   createContext<DropzoneContextValue>({
     name: 'DropzoneContext',
